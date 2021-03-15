88b9f9455a59bee04d7b3d5501a0884397bffa43
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-03-15 22:32:04 +0600

    sceond commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./firebase.comfig";
+import { useState } from "react";
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app(); // if already initialized, use that one
+}
 
 function App() {
+  const [user, setUser] = useState({});
+  var provider = new firebase.auth.GoogleAuthProvider();
+  var fbProvider = new firebase.auth.FacebookAuthProvider();
+  var ghProvider = new firebase.auth.GithubAuthProvider();
+  var yhProvider = new firebase.auth.OAuthProvider("yahoo.com");
+  const handleGoogleSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        var credential = result.credential;
+        var token = credential.accessToken;
+        var user = result.user;
+        console.log(user);
+        setUser(user);
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(errorCode, errorMessage, email, credential);
+      });
+  };
+  const handleFacebookSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(fbProvider)
+      .then((result) => {
+        var credential = result.credential;
+        var user = result.user;
+        var accessToken = credential.accessToken;
+        console.log("fb user", user);
+        setUser(user);
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(errorCode, errorMessage, email, credential);
+      });
+  };
+  const handleGithubSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(ghProvider)
+      .then((result) => {
+        var credential = result.credential;
+        var token = credential.accessToken;
+        var user = result.user;
+        setUser(user);
+        console.log("Gh user");
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(errorCode, errorMessage, email, credential);
+      });
+  };
+  const handleYahooSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(yhProvider)
+      .then((result) => {
+        const credential = result.credential;
+        var accessToken = credential.accessToken;
+        var user = result.user;
+        console.log('Yahoo',user)
+        setUser(user)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSignIn}>Sign In using Google</button>
+      <br />
+      <button onClick={handleFacebookSignIn}>Sign In using Facebook</button>
+      <br />
+      <button onClick={handleGithubSignIn}>Sign In using Github</button>
+      <br />
+      <button onClick={handleYahooSignIn}>Sign In using Yahoo</button>
+      <h4>{user.displayName}</h4>
+      <p>Your Email: {user.email}</p>
+      <p>
+        <small>Your Profile Image</small>
+      </p>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
---
+++ src/firebase.comfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB1fpVle1eaG9zuooR_HLuwGJTodI4etDY",
+    authDomain: "auth-recap-ce528.firebaseapp.com",
+    projectId: "auth-recap-ce528",
+    storageBucket: "auth-recap-ce528.appspot.com",
+    messagingSenderId: "235389724953",
+    appId: "1:235389724953:web:65071029caef1fe318fd88"
+  };
+  export default firebaseConfig;
